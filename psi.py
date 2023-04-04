17e3b49b59a53fb803cbf1dd870aae363e812348
Dennis <101068960+dennisdeh@users.noreply.github.com>
2023-04-05 00:25:04 +0200

    Update psi.py
    small change to structure

--- psi.py
+++ psi.py
@@ -112,25 +112,3 @@ def psi_calc(X1: Union[list, np.ndarray, pd.Series],
 
     return psi_value, df0
 
-
-# %%
-if __name__ == "__main__2":
-    # realistic example
-    X1 = np.array([1, 2, 3, 3, 5, 4, 2, 2, 3, 2, 3, 4, 2, 2, 2, 5, 4, 5, 7, np.nan, 7])
-    X2 = np.array([1, 2, 2, 3, 5, 4, 2, np.nan, 3, 2, np.nan, 4, 2, 2, 2, 5, 3, 5, 6, np.nan])
-
-    psi_calc(X1=X1, X2=X2, method_num_feature="equidistant", bins=3)
-    psi_calc(X1=X1, X2=X2, method_num_feature="quantiles", bins=3)
-    psi_calc(X1=X1, X2=X2, force_categorical=True)
-
-    # identical
-    X1 = np.array([1, 2, 3, 3, 5, 4, 2, 2, 3, 2, 3, 4, 2, 2, 2, 5, 4, 5, 7, np.nan, 7])
-    X2 = X1
-
-    psi_calc(X1=X1, X2=X2, method_num_feature="equidistant", bins=3)
-
-    # completely different
-    X1 = [0.0, 0.1] * 100
-    X2 = [1.0, 1.1] * 120
-
-    psi_calc(X1=X1, X2=X2, method_num_feature="equidistant", bins=3)
